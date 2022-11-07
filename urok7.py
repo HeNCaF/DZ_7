7bf776535f08c6c03489feca246f7f0e442864af
HeNCaF <116014077+HeNCaF@users.noreply.github.com>
2022-11-07 19:16:36 +0200

    Update urok7.py

--- urok7.py
+++ urok7.py
@@ -45,40 +45,40 @@
 # iterator = iter(list)
 # for elem in iterator:
 #     print(elem)
-# class Counter:
-#     def __init__(self, max_number):
-#         self.i = 0
-#         self.max_number = max_number
-#     def __iter__(self):
-#         self.i = 0
-#         return self
-#     def __next__(self):
-#         self.i += 1
-#         def kvadrat(number, max_degree):
-#             i = 0
-#             for __ in range(max_degree):
-#                 yield number ** i
-#                 i += 1
-#         res = kvadrat(self.i, self.max_number)
-#         print(res)
-#         for _ in res:
-#             print(_)
-#         if self.i > self.max_number:
-#             raise StopIteration
-#         return self.i
-# count = Counter(5)
-# for elem in count:
-#     print(elem)
-def checker(function):
-    def checkef(*args, **kwargs):
-        try:
-            result = function(*args, **kwargs)
-        except Exception as exc:
-            print(f"Problem with {exc}")
-        else:
-            print(f"No problem. Result - {result}")
-    return checker
-@checker
-def calculate(expression):
-    return eval(expression)
-calculate("2+2")
+ class Counter:
+     def __init__(self, max_number):
+         self.i = 0
+         self.max_number = max_number
+     def __iter__(self):
+         self.i = 0
+         return self
+     def __next__(self):
+         self.i += 1
+         def kvadrat(number, max_degree):
+             i = 0
+             for __ in range(max_degree):
+                 yield number ** i
+                 i += 1
+         res = kvadrat(self.i, self.max_number)
+         print(res)
+         for _ in res:
+             print(_)
+         if self.i > self.max_number:
+             raise StopIteration
+         return self.i
+ count = Counter(5)
+ for elem in count:
+     print(elem)
+#def checker(function):
+#   def checkef(*args, **kwargs):
+#        try:
+#            result = function(*args, **kwargs)
+#        except Exception as exc:
+#            print(f"Problem with {exc}")
+#        else:
+#            print(f"No problem. Result - {result}")
+#    return checker
+#@checker
+#def calculate(expression):
+#    return eval(expression)
+#calculate("2+2")
